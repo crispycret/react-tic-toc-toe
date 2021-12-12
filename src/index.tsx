8bf183e410bf369ffd8fef7f644a14218bca1d25
Brandon Nadeau <brandonmnadeau@hotmail.com>
2021-12-12 16:52:14 -0500

    Setup
    Installed dependencies and added notes to index.tsx on how to import the mui library.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import App from './App';
 
+
+/*
+ * mui branch of tic-tac-toe
+ * 
+ * To install mui use the following command:
+ * 
+ * yarn add @mui/material @emotion/react @emotion/styled @mui/icons-material
+ * 
+ * and add the following lines to index.html in the public folder with the other <link> tags.
+ * 
+ *   <!--
+      Roboto font from https://mui.com/getting-started/installation/.
+    -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+    
+    <!--
+      Font icons from https://mui.com/getting-started/installation/.
+    -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
+ * 
+*/
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
